a6d49099fffa4fe01c85d8f489331ad789d2f93d
connosfs <connosfs@googlemail.com>
2026-04-21 20:15:48 +0200

    Delete amazon-extension directory

--- amazon-extension/background.js
+++
@@ -1,8 +0,0 @@
-// Amazon Spending Analysis / Ausgaben-Analyse – Background Service Worker
-// EN: Handles inter-component messaging (opens dashboard tab on request)
-// DE: Verwaltet Kommunikation zwischen Komponenten (öffnet Dashboard-Tab auf Anfrage)
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-  if (msg.action === 'openDashboard') {
-    chrome.tabs.create({ url: chrome.runtime.getURL('dashboard.html') });
-  }
-});
